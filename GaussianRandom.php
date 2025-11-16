5862dc79fe759add32efb8033a5e52b6d81b74c4
prostriker87 <36673553+prostriker87@users.noreply.github.com>
2025-11-16 16:57:33 +0100

    Create GaussianRandom.php

---
+++ GaussianRandom.php
@@ -0,0 +1,26 @@
+class GaussianRandom {
+
+        /**
+         * Genera un número aleatorio con distribución normal (gaussiana)
+         * usando la transformación de Box–Muller.
+         *
+         * @param float $mean  Media (mu)
+         * @param float $sigma Desviación estándar (sigma)
+         * @return float Número generado
+         */
+        public static function generate($mean = 0.0, $sigma = 1.0) {
+
+            // Box-Muller
+            do { 
+                $u1 = mt_rand() / mt_getrandmax();
+            } while ($u1 == 0);
+
+            do {
+                $u2 = mt_rand() / mt_getrandmax();
+            } while ($u2 == 0);
+
+            $z0 = sqrt(-2.0 * log($u1)) * cos(2.0 * M_PI * $u2);
+
+            return $z0 * $sigma + $mean;
+        }
+    }
