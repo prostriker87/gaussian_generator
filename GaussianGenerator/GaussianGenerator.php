ffebe34526481fc22c68ec0ebafb98487d61c8d0
prostriker87 <36673553+prostriker87@users.noreply.github.com>
2025-11-17 01:16:56 +0100

    Remove unauthorized access check from GaussianGenerator

--- GaussianGenerator/GaussianGenerator.php
+++ GaussianGenerator/GaussianGenerator.php
@@ -1,8 +1,6 @@
 <?php 
-    if(!defined($sitename)) {
-        die('Acceso no autorizado.');
-    }
     include '_scripts/GaussianGenerator/_include/GaussianRandom.php';
     include '_scripts/GaussianGenerator/_include/GaussianProbability.php';
     include '_scripts/GaussianGenerator/_include/WeightedGaussianGenerator.php';
 ?>
+
