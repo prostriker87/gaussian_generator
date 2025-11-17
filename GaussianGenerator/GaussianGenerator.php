492ae829c73e8eed6365399dcf577b39f059d989
prostriker87 <36673553+prostriker87@users.noreply.github.com>
2025-11-17 01:16:43 +0100

    Refactor includes and access check in GaussianGenerator

--- GaussianGenerator/GaussianGenerator.php
+++ GaussianGenerator/GaussianGenerator.php
@@ -1,11 +1,8 @@
 <?php 
-    if(!defined($sitename)){
+    if(!defined($sitename)) {
         die('Acceso no autorizado.');
     }
     include '_scripts/GaussianGenerator/_include/GaussianRandom.php';
-    LoadingTime::mark('GaussianRandom.php');
     include '_scripts/GaussianGenerator/_include/GaussianProbability.php';
-    LoadingTime::mark('GaussianProbability.php');
     include '_scripts/GaussianGenerator/_include/WeightedGaussianGenerator.php';
-    LoadingTime::mark('WeightedGaussianGenerator.php');
 ?>
